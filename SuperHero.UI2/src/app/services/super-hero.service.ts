c2150a5939a49ca3f5be79679a6f5077d9130c21
togo88-global <tiagogb_2@hotmail.com>
2022-06-30 18:15:00 -0300

    commit ui 2

---
+++ SuperHero.UI2/src/app/services/super-hero.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { SuperHero } from '../models/super-hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuperHeroService {
+  private url = "SuperHero";
+  constructor(private http: HttpClient) { }
+
+  public getSuperHeroes(): Observable<SuperHero[]> {  
+    console.log(`URL API: ${environment.apiUrl}/${this.url}`);
+    return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
+  }
+
+  public updateHero(hero: SuperHero): Observable<SuperHero[]> {  
+    console.log(`URL API: ${environment.apiUrl}/${this.url}`);
+    return this.http.put<SuperHero[]>(`${environment.apiUrl}/${this.url}`, hero);
+  }
+
+  public createHero(hero: SuperHero): Observable<SuperHero[]> {  
+    console.log(`URL API: ${environment.apiUrl}/${this.url}`);
+    return this.http.post<SuperHero[]>(`${environment.apiUrl}/${this.url}`, hero);
+  }
+
+  public deleteHero(hero: SuperHero): Observable<SuperHero[]> {  
+    console.log(`URL API: ${environment.apiUrl}/${this.url}`);
+    return this.http.delete<SuperHero[]>(`${environment.apiUrl}/${this.url}/${hero.id}`);
+  }
+}
